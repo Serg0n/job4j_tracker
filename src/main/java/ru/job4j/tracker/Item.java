17af0090f9a0f2a309a3bcb1180e749ed9d2dcf3
Serg0n <cyppe135@gmail.com>
2024-11-05 20:52:41 +0300

    1. Чтение кода и базовые ошибки [#40205 #520425]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,12 +6,15 @@ public class Item {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
