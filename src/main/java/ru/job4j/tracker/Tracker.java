8c80a92b412b177a15094700d37054f8eee6e9ec
Serg0n <cyppe135@gmail.com>
2024-11-29 15:57:41 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,39 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        int size = 0;
+        Item[] result = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name != null) {
+                result[size] = name;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        for (int index = 0; index < result.length; index++) {
+            System.out.println(result[index]);
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] result = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null && key.equals(items[index].getName())) {
+                result[size] = items[index];
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        for (Item item : result) {
+            System.out.println(item != null ? item.getName() : "null");
+        }
+        return result;
     }
 
     public Item findById(int id) {
