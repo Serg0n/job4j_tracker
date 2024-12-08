899c499d5cf73ca3b73c0e1bdf36c965c520e6df
Serg0n <cyppe135@gmail.com>
2024-12-08 22:17:11 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,4 +55,16 @@ public class Tracker {
         }
         return false;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return;
+        }
+        int start = index + 1;
+        int length = size - index - 1;
+        items[size - 1] = null;
+        size--;
+            System.arraycopy(items, start, items, index, length);
+    }
 }
