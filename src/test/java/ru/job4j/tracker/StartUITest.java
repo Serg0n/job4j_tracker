3a909bbfa9a921fc5fcfe2e4a8d551d3a9547c1f
Serg0n <cyppe135@gmail.com>
2024-12-19 23:05:37 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+/*package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -54,4 +54,4 @@ class StartUITest {
         Item deleted = tracker.findById(item.getId());
         assertThat(deleted).isNull();
     }
-}
+}*/
