addbff4b9fe25a5041dca912b60a736e020dc5fb
Serg0n <cyppe135@gmail.com>
2024-12-19 23:57:42 +0300

    12.1 Зависимость от System.out [#33568]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-    @Test
+    /*@Test
     void whenCreateItem() {
         Input input = new MockInput(
                 new String[] {"0", "Item name", "1"}
@@ -38,7 +38,7 @@ class StartUITest {
     void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item")); /* Добавляется в tracker новая заявка */
-        Input input = new MockInput(
+     /*/   Input input = new MockInput(
                 new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
@@ -47,5 +47,24 @@ class StartUITest {
         };
         new StartUI().init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
+    }
+*/
+
+    @Test
+    void whenExit() {
+        Output output = new StubOutput();
+        Input input = new MockInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + System.lineSeparator()
+                        + "0. Завершить программу" + System.lineSeparator()
+                        + "=== Завершение программы ===" + System.lineSeparator()
+        );
     }
 }
