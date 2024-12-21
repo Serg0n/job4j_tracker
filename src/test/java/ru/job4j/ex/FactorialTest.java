0817442438bda5a89a985a6a61c72773c9bf9e09
Serg0n <cyppe135@gmail.com>
2024-12-21 18:15:36 +0300

    0.3. Тестирование исключений с junit [#219366 #525988]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -16,4 +16,11 @@ public class FactorialTest {
         );
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    public void whenPositiveNumberThenThrowException() {
+        Factorial factorial = new Factorial();
+        int result = factorial.calc(5);
+        assertThat(result).isEqualTo(120);
+    }
 }
