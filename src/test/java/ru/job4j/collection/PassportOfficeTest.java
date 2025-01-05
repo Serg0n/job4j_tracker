977e6832409414550fbe866fad6823fcc1b2703c
Serg0n <cyppe135@gmail.com>
2025-01-05 21:04:12 +0300

    1. Паспорт и жители [#212656 #527328]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,14 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestDoublePassport() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivan Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        boolean result = office.add(citizen2);
+        assertThat(result).isFalse();
+    }
 }
