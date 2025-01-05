7d8c93782ea3c8561a961eb537829f51ce1ce930
Serg0n <cyppe135@gmail.com>
2025-01-05 20:42:55 +0300

    0. Map, HashMap [#212652 #527325]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,13 +1,23 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class UsageMap {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("zark0.n@yandex.ru", "Невский Сергей Александрович");
-        map.put("vasya.pupkin@mail.com", "Некрасов Василий Юрьевич");
-        map.put("parsentev@yandex.ru", "Arsentev Petr Sergeevich");
+        HashMap<String, List<String>> map = new HashMap<>();
+        map.put("zark0.n@yandex.ru", new ArrayList<>());
+        map.get("zark0.n@yandex.ru").add("Сергей");
+        map.get("zark0.n@yandex.ru").add("Невский Сергей Александрович");
+
+        map.put("vasya.pupkin@mail.com", new ArrayList<>());
+        map.get("vasya.pupkin@mail.com").add("Некрасов Василий Юрьевич");
+
+        map.put("parsentev@yandex.ru", new ArrayList<>());
+        map.get("parsentev@yandex.ru").add("Petr");
+        map.get("parsentev@yandex.ru").add("Arsentev Petr Sergeevich");
+
         for (String key : map.keySet()) {
             System.out.println("Почта: " + key + ", ФИО: " + map.get(key));
         }
