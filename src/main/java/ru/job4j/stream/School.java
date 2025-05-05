1caa778a03f25a25058ad0b2979c0b052bfdc073
nevskysa-intelins-ru <cyppe135@gmail.com>
2025-05-05 16:47:20 +0300

    5. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public ArrayList<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return (ArrayList<Student>) students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+}
